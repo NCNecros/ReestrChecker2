d58775d34f0884a54168d4467e5a5582d325eb59
Stanislav Naydeny <n.c.necros@gmail.com>
2017-02-08 12:36:31 +0300

    подправлена загрузка спр69

--- src/main/java/company/DBFHelper.java
+++ src/main/java/company/DBFHelper.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Resource;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -18,6 +19,15 @@ public class DBFHelper {
     DbfReader reader;
     @Resource
     private DataFiller dataFiller;
+
+    public List<Spr69Value> getSpr69List() {
+        return spr69List;
+    }
+
+    public void setSpr69List(List<Spr69Value> spr69List) {
+        this.spr69List = spr69List;
+    }
+
     @Resource
     private List<Spr69Value> spr69List;
 
@@ -60,7 +70,9 @@ public class DBFHelper {
     void readFromSpr69() throws FileNotFoundException {
         DbfRow row;
         reader = new DbfReader(new FileInputStream(getClass().getClassLoader().getResource("SPR69.dbf").getFile()));
-
+        if (spr69List== null){
+            spr69List = new ArrayList<>(100000);
+        }
         while ((row = reader.nextRow()) != null) {
             spr69List.add(EntityFactory.buildSpr69ValueFromRow(row));
         }
